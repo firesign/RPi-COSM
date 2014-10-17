154292f393843dde6c7142dfb982207bc76c77fa
Michael LeBlanc <ml@generaleccentric.net>
2014-10-17 10:15:43 -0300

    Added Kitchen data

--- cosm.py
+++ cosm.py
@@ -5,6 +5,16 @@
 # generaleccentric.net
 #
 # May, 2013
+# Last edit October 2014
+
+# Nodes:
+#   1: Office  
+#   2: MBL_room
+#   3: In-Law_Suite
+#   4: Pantry
+#   5: Remote (shed)
+#  10: Garage
+#  11: Kitchen
 
 from __future__ import division
 
@@ -84,7 +94,7 @@ def main():
     return temperature               # pass this on!
 
   
-  nodeList = 'Office', 'Garage', 'MBL_Room', 'In-law_Suite', 'Node4', 'Battery_Test'
+  nodeList = 'Office', 'Garage', 'MBL_Room', 'In-law_Suite', 'Pantry', 'Kitchen'
 
   # CPU monitor routine, requires "import os"
   def getCPUtemperature():
@@ -436,6 +446,67 @@ def main():
 
       else:
         rr = 0
+        
+      if node == '11':
+        
+        tempLow = rawx.pop(0).strip()           # temperature low byte
+        tempHigh = rawx.pop(0).strip()          # temperature high byte
+        if int(tempHigh) < 5:			# if this byte is greater than 5, 
+        				# temp is (for sure) a positive value
+          tempRaw = (int(tempHigh) * 256) + int(tempLow)
+        else:
+          tempRaw = (255 ^ (int(tempLow))) * -1	# temp is negative value, so do a 1's complement
+        tempRaw2 = tempRaw + 0.0
+        actualTemp = tempRaw2 / 10
+        
+        humLow = rawx.pop(0).strip()            # humidity low byte
+        humHigh = rawx.pop(0).strip()           # humidity high byte
+        humRaw = (int(humHigh) * 256) + int(humLow)
+        humRaw2 = humRaw + 0.0
+        actualHum = humRaw2 / 10
+        
+        baroLow = rawx.pop(0).strip()            # baro low byte
+        baroMid = rawx.pop(0).strip()            # baro mid byte
+        baroHigh = rawx.pop(0).strip()           # baro high byte
+        throwaway = rawx.pop(0).strip()          # throw away this byte
+        baroRaw = (int(baroHigh) * 65536) + (int(baroMid) * 256) + int(baroLow)
+        actualBaro = (baroRaw + 1200) / 1000.0
+        
+        bvLow = rawx.pop(0).strip()              # batt volt low byte
+        bvMid = rawx.pop(0).strip()              # batt volt mid byte
+        bvHigh = rawx.pop(0).strip()             # batt volt high byte
+        throwaway = rawx.pop(0).strip()          # throw away this byte
+        bvRaw = (int(bvHigh) * 65536) + (int(bvMid) * 256) + int(bvLow)
+        actualBv = bvRaw * 3.3 / 1024
+        
+        
+        
+        pac.update([eeml.Data('Kitchen_Temp', actualTemp,
+                              unit=eeml.Unit('celcius', 'basicSI', 'C')),
+                    eeml.Data('Kitchen_Humidity', actualHum,
+                              unit=eeml.Unit('percent', 'basicSI', 'RH')),
+                    eeml.Data('Kitchen_Barometer', actualBaro,
+                              unit=eeml.Unit('pressure', 'basicSI', 'kPa')),
+                    eeml.Data('Kitchen_BattVolt', actualBv,
+                              unit=eeml.Unit('voltage', 'basicSI', 'v'))])
+        
+        try:
+          pac.put()
+        except Exception as e:
+          print "Oops! Something went wrong. Error = {}".format(e)
+        except CosmError, e:
+          now = datetime.datetime.now()
+          print now.strftime('%Y-%m-%d %H:%M ERROR Node 2: StandardError')
+          print('ERROR: pac.put(): {}'.format(e))
+        except StandardError:
+          #print('ERROR: StandardError')
+          now = datetime.datetime.now()
+          print now.strftime('%Y-%m-%d %H:%M ERROR Node 2: StandardError')
+        except:
+          now = datetime.datetime.now()
+          print now.strftime('%Y-%m-%d %H:%M ERROR Node 2: StandardError')
+          print('ERROR: Unexpected error: %s' % sys.exc_info()[0])  
+  
 
 # call the main() function
 if __name__ == '__main__':
